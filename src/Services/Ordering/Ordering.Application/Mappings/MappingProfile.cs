19a207526bbb6fdbe7d3dbbc6b80fb8f2ca39c44
Elmar <elmar.mammadov@gts.az>
2021-07-17 18:47:53 +0400

    after some changes

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.CheckoutOrder.Commands;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
 using System;
@@ -14,7 +16,8 @@ namespace Ordering.Application.Mappings
         public MappingProfile()
         {
             CreateMap<Order, OrdersVm>().ReverseMap();
-          
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
 }
